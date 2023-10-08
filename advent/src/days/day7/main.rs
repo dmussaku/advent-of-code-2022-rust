4bb55eb5068882f7fb77426865e63ce366e3c34b
dmussaku <dmussaku@gmail.com>
2023-10-08 17:16:31 +0200

    Add nonworking solution day7

--- advent/src/days/day7/main.rs
+++ advent/src/days/day7/main.rs
@@ -7,7 +7,7 @@ struct Directory{
     name: String,
     files: Vec<File>,
     directories: Vec<DirectoryRef>,
-    parent_position: Option<usize>,
+    parent: Option<DirectoryRef>,
 }
 
 impl Directory {
@@ -59,16 +59,14 @@ pub fn read_input_from_file(path: &str) -> Vec<String>{
 }
 
 
-fn create_directory_tree(input: Vec<String>) -> Vec<DirectoryRef>{
+fn create_directory_tree(input: Vec<String>) -> Directory{
     let mut root = Directory{
         name: "/".to_string(),
         files: Vec::new(),
         directories: Vec::new(),
-        parent_position: None,
+        parent: None,
     };
-    let mut directory_tree: Vec<DirectoryRef> = Vec::new();
-    directory_tree.push(Rc::new(RefCell::new(root.clone())));
-    let mut current_directory_position: usize = 0;
+    let mut current_directory = Rc::new(RefCell::new(root.clone()));
 
     for line in input.iter().skip(1){
         println!("Line: {}", line);
@@ -81,19 +79,13 @@ fn create_directory_tree(input: Vec<String>) -> Vec<DirectoryRef>{
                     // we change the current directory to the parent directory
                     // if the current directory is the root directory, we do nothing
                     // current_directory = &directory_tree[current_directory.borrow().parent_position.unwrap()]
-                    current_directory_position = directory_tree[current_directory_position]
-                        .borrow().parent_position.unwrap();
+                    current_directory = current_directory.borrow().parent.clone().unwrap();
                 }
                 else{ // eg $ cd a
                     // If the command is to change directory to a new directory
                     // we create a new directory and make it's parent the current directory
                     // the we change the current directory to this new directory created
-                    let current_directory = directory_tree[current_directory_position]
-                        .borrow()
-                        .find_directory(command[2]);
-                    if current_directory.is_some(){
-                        current_directory_position = directory_tree.iter().position(|directory| directory.borrow().name == command[2]).unwrap();
-                    }
+                    current_directory = current_directory.borrow().find_directory(command[2]).unwrap();
                 }
             }
             else if command[1] == "ls"{
@@ -103,31 +95,31 @@ fn create_directory_tree(input: Vec<String>) -> Vec<DirectoryRef>{
         else {
             let command = line.split_whitespace().collect::<Vec<&str>>();
             if command[0] == "dir"{
-                let current_directory = directory_tree[current_directory_position].borrow_mut();
+                let mut current_directory = current_directory.borrow();
                 if !current_directory.is_directory_exist(command[1]){
                     let new_directory = Directory{
                         name: command[1].to_string(),
                         files: Vec::new(),
                         directories: Vec::new(),
-                        parent_position: Some(current_directory_position.clone()),
+                        parent: Some(Rc::new(RefCell::new(current_directory.clone()))),
                     };
-                    directory_tree[current_directory_position].borrow_mut().directories.push(Rc::new(RefCell::new(new_directory.clone())));
-                    directory_tree.borrow_mut().push(Rc::new(RefCell::new(new_directory.clone())));
+                    // push new_directory into current_directory.directories
+                    current_directory.borrow_mut().directories.push(Rc::new(RefCell::new(new_directory)));
                 }
             }
             else {
-                let current_directory = directory_tree[current_directory_position].borrow_mut();
+                let current_directory = current_directory.borrow();
                 if !current_directory.is_file_exists(command[1]){
                     let new_file = File{
                         name: command[1].to_string(),
                         size: command[0].parse::<usize>().unwrap(),
                     };
-                    directory_tree[current_directory_position].borrow_mut().files.push(new_file.clone());
+                    current_directory.files.push(new_file);
                 }
             }
         }
     }
-    directory_tree
+    root
 }
 
 // pub fn run_part_1(input: String) -> usize{
@@ -152,13 +144,9 @@ mod tests{
     #[test]
     fn test_create_directory_tree(){
         let input = vec!["$ cd /", "$ ls", "dir a", "14848514 b.txt", "8504156 c.dat", "dir d", "$ cd a", "$ ls", "dir e", "29116 f", "2557 g", "62596 h.lst", "$ cd e", "$ ls", "584 i", "$ cd ..", "$ cd ..", "$ cd d", "$ ls", "4060174 j", "8033020 d.log", "5626152 d.ext", "7214296 k"];
-        let results = create_directory_tree(input.iter().map(|line| line.to_string()).collect::<Vec<String>>());
-        // println!("{:?}", results);
-        for result in results.iter(){
-            println!("{:?}", result.borrow());
-        }
-        assert_eq!(results[0].borrow().name, "/");
-        assert_eq!(results[0].borrow().files.len(), 2);
+        let root = create_directory_tree(input.iter().map(|line| line.to_string()).collect::<Vec<String>>());
+        assert_eq!(root.name, "/");
+        assert_eq!(root.files.len(), 2);
     }
 
     // #[test]
